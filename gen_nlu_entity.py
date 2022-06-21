bbfd290be15fbd99a2a1e31c147ec59aa5a278d0
Jader Martins <jadermcs94@gmail.com>
2022-06-21 20:50:56 -0300

    improve stories, regression tests

--- gen_nlu_entity.py
+++ gen_nlu_entity.py
@@ -12,9 +12,17 @@ def main():
     with open("data/back.nlu.yml") as fin:
         data = fin.read()
     data = yaml.load(data)
-    pedidos = data["nlu"][1]["examples"].split("\n")
-    proc = [reply.replace("{itens}", f"[{genp()}](itens)") for reply in pedidos]
-    data["nlu"][1]["examples"] = "\n".join(proc)
+    # pedidos = data["nlu"][2]["examples"].split("\n")
+    # proc = [reply.replace("{endereco}",
+    #         f"[{pizza_utils.slot_value_endereco()}](endereco)") for reply in pedidos]
+    # data["nlu"][2]["examples"] = "\n".join(proc)
+    # pedidos = data["nlu"][1]["examples"].split("\n")
+    # proc = [reply.replace("{itens}", f"[{genp()}](itens)") for reply in pedidos]
+    # data["nlu"][1]["examples"] = "\n".join(proc)
+    pedidos = data["nlu"][3]["examples"].split("\n")
+    proc = [reply.replace("{endereco}",
+            f"[{pizza_utils.slot_value_endereco()}](endereco)") for reply in pedidos]
+    data["nlu"][3]["examples"] = "\n".join(proc)
     with open("data/nlu.yml", "w") as fout:
         yaml.dump(data, fout)
 
